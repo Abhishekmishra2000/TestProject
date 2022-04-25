5739faccd788b6c4c9603db65a89ddd14d3fab8b
M1083075 <M1083075@MT-PF1ZSTS4>
2022-04-25 11:40:27 +0530

    First commit

---
+++ JavaBasics/src/basics/EvenNumbers.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int number=100;
+System.out.print("List of Even numbers from 20 to "+number+" : ");
+for(int i=20;i<=number;i++) {  //logic to be checked if the number is even or not
+	if(i%2==0)  //if i%2 is equal to ZERO, the number is even
+	{
+		System.out.print(i+" ");
+		
+	}
+	
+}
+	}
+
+}
---
+++ JavaBasics/src/basics/Printnumbers.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Printnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=20;
+System.out.print("Numbers from 1 to"+ n +"are :");
+for (int i=1;i<=n;i++) {
+	System.out.print(i+" ");
+}
+	}
+
+}
