8d99b6a089cbcb4c60f169c86c1ac66a2242ed8a
M1083075 <M1083075@MT-PF1ZSTS4>
2022-04-25 12:11:39 +0530

    java program

---
+++ Javaprograms/src/arraylist/Employee.java
@@ -0,0 +1,24 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class Employee {
+ public Employee(int i, String string, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+public static void main(String []args) {
+	 ArrayList<Employee> emplist = new ArrayList<>();
+	 Employee emp1 = new Employee(1,"Abhi",15000);
+	 
+	 Employee emp2 = new Employee(2,"Amit",10000);
+
+	 Employee emp3 = new Employee(3,"Riya",16000);
+
+	 Employee emp4 = new Employee(4,"Khusbu",20000);
+
+	 Employee emp5 = new Employee(5,"Seenu",25000);
+
+	 
+ }
+}
